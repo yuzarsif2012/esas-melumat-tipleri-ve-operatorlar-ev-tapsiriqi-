e85edc4221c3012c82b4beca22ccb103684f39ee
YUZARSIF2013 <quliyevy010@gmail.com>
2025-09-17 21:44:19 +0400

    ev tapsiriqi operatorlar

---
+++ app.js
@@ -0,0 +1,23 @@
+let telebe = {
+    Ad: "Yuzarsif",
+    Soyad: "Esgerli",
+    Yas: 12,
+    Qurup: "It-204",
+}
+
+console.log(telebe);
+
+if (telebe.Yas < 18) {
+    console.log("Giris Qadagandir");
+} else {
+    console.log("Xos Geldiniz")
+}
+
+
+
+let a = 20;
+let b = 6;
+let hasil = a * b;// 120
+let qaliq = a % b;// 2
+console.log("Hasil" + hasil);
+console.log("Qaliq" + qaliq);
